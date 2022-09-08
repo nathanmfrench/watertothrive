599ba8c8897a63fdd8609fecf98418ba49add23c
Nathan French <nathanmfrench17@gmail.com>
2022-09-08 11:28:18 -0400

    Update Maps.jsx

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,33 +1,36 @@
-import React from 'react';
-
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import { Component } from 'react';
-
-const mapStyles = {
-  
-};
-
-
-export class Maps extends Component {
-  render() {
-    return (
-      <div className='container1'>
-        <Map style={"position:absolute; width:60%; height:100%;"}
-          google={this.props.google}
-          zoom={6}
-          style={mapStyles}
-          initialCenter={{
-            lat: 9,
-            lng: 39
-        }} />
-        <div className='projects_sidebar'>
-        projects
-        </div>
-      </div>);
-
-  }
-}
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAniSzdehDYrZ0E95aMIh_X1Ctxm9lW0OA'
-})(Maps);
+  apiKey: "AIzaSyAniSzdehDYrZ0E95aMIh_X1Ctxm9lW0OA"
+})(function App(props) {
+  return (
+    <div>
+      <div
+        style={{
+          overflow: "hidden",
+          display: "grid",
+          gridTemplateColumns: "0px 1fr",
+          width: "100%",
+          height: "100vh"
+        }}
+      >
+        <div></div>
+        <div
+          style={{
+            position: "relative"
+          }}
+        >
+          <Map
+            style={{ position: "absolute", width: "100%", height: "100%" }}
+            google={props.google}
+            zoom={6}
+            initialCenter={{
+              lat: 9,
+              lng: 39
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+});
